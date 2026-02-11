4ccd3cafef348c98dead3ef3f4f95b02536d7987
Ricko124 <123193828+Ricko124@users.noreply.github.com>
2026-02-11 23:46:44 +0100

    Add LuxuAdmin FiveM admin menu resource

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'Codex'
+description 'LuxuAdmin - Simple FiveM admin menu'
+version '1.0.0'
+
+shared_script 'config.lua'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
